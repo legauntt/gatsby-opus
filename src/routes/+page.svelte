c69533b25ea0f9bcdeb92546dcb546ab32db46ec
TheGauntt <jessethegauntt+biz@gmail.com>
2025-04-08 16:58:02 -0700

    Basics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let daTroof = $state(false);
+
+	const heh = () => {
+		const wup = new Audio('/dvdp/06_someday.m4a');
+		wup.play();
+		daTroof = true;
+	};
+</script>
+
+<div class="p-5">
+	<div class="text-2xl">Apinkslip</div>
+
+	<div class="my-5">
+		<button class="border border-solid border-black bg-violet-400 p-2" onclick={heh}>
+			Yehrye
+		</button>
+	</div>
+
+	{#if daTroof}
+		<div>Abzolutely</div>
+	{/if}
+</div>
+
+<style scoped>
+</style>
