757c63c28923b1867d0299e5b63a7a7231289807
TheGauntt <jessethegauntt+biz@gmail.com>
2025-04-08 17:53:41 -0700

    My balls are standing up on end

---
+++ src/lib/tracklist.ts
@@ -0,0 +1,16 @@
+export const TRACKLIST = [
+	'01_whoir.m4a',
+	'02_glosky.m4a',
+	'03_dabez.m4a',
+	'04_balls.m4a',
+	'05_nchain.m4a',
+	'06_someday.m4a',
+	'07_made.m4a',
+	'08_road.m4a',
+	'09_novipain.m4a',
+	'10_gravity.m4a',
+	'11_medusa.m4a',
+	'12_rounds.m4a',
+	'13_party.m4a',
+	'bonus_whale.m4a'
+].map((entry) => `/dvdp/${entry}`);
