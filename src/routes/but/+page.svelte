7dbe09b2846bdf476d4e445b1246e3d692556ca2
TheGauntt <jessethegauntt+biz@gmail.com>
2025-04-11 13:25:01 -0700

    Pullin' and dreams

--- src/routes/but/+page.svelte
+++ src/routes/but/+page.svelte
@@ -4,14 +4,60 @@
 	let audioElm: HTMLAudioElement;
 	let woahing = $state(false);
 	let moodyCount = $state(0);
+	let dreams = $state(0);
+
+	let dreamsElm: HTMLDivElement;
+
+	let dreamsQueue: HTMLAudioElement[] = $state([]);
+
+	let lastDream: number | null = $state(null);
+
+	$effect(() => {
+		if (!dreamsElm) {
+			return;
+		}
+
+		dreamsElm.style.width = dreams * 10 + '%';
+	});
 
 	const forceAudio = () => {
 		if (audioElm && audioElm.paused) {
 			audioElm.play();
+		} else {
+			if (dreams >= 10) {
+				return;
+			}
+
+			const dreamsTrack = new Audio('/butts/dreams.mp3');
+			if (dreams > 0) {
+				dreamsQueue.push(dreamsTrack);
+			} else {
+				dreamsTrack.play();
+				lastDream = Date.now();
+			}
+
+			dreams++;
+
+			dreamsTrack.addEventListener('ended', () => {
+				dreams--;
+			});
 		}
 	};
 
 	setInterval(() => {
+		if (audioElm?.paused) {
+			return;
+		}
+
+		if (dreamsQueue.length > 0) {
+			if (Date.now() - (lastDream || 0) >= 5000) {
+				let dreamTrack = dreamsQueue.shift();
+				console.log(`Dripping in a queued dream.`);
+				dreamTrack?.play();
+				lastDream = Date.now();
+			}
+		}
+
 		if (!woahing && random(1, 255) == 255) {
 			const woah = new Audio('/butts/woah.mp3');
 			woahing = true;
@@ -22,6 +68,23 @@
 			});
 
 			console.log(new Date(), 'added woah');
+		}
+
+		if (random(1, 69) == 69) {
+			console.log(new Date(), 'added pullin flow');
+
+			const pullingBiggest = new Audio('/butts/pullin_doublexl.mp3');
+			pullingBiggest.play();
+
+			pullingBiggest.addEventListener('ended', () => {
+				const pullinLarge = new Audio('/butts/pullin_large.mp3');
+				pullinLarge.play();
+
+				pullinLarge.addEventListener('ended', () => {
+					const pullin = new Audio('/butts/pullin.mp3');
+					pullin.play();
+				});
+			});
 		}
 
 		if (random(1, 69) == 69) {
@@ -53,6 +116,10 @@
 			{moodyCount}
 		</div>
 	{/if}
+
+	<div class="absolute right-5 bottom-5 text-lg text-slate-500">2025-04-11 1pm</div>
+
+	<div bind:this={dreamsElm} class="absolute top-100 left-0 h-4 bg-red-500"></div>
 
 	{#if woahing}
 		<div class="absolute top-1/2 left-1/3 text-7xl text-red-500">WOOOOOOOOOOAH</div>
