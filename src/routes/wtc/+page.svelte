8355bead42e369313b414506ae516883438f27e0
TheGauntt <jessethegauntt+biz@gmail.com>
2025-04-16 20:28:52 -0700

    Close

--- src/routes/wtc/+page.svelte
+++ src/routes/wtc/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+	import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
+	import { pushState } from '$app/navigation';
+	import axios from 'axios';
 	import { page } from '$app/state';
 	import { onMount } from 'svelte';
 	import { TREASURE_TROVE } from '$lib/cetlist';
 	import Wavey from '$lib/comps/wavey.svelte';
 	import { formatTrack } from '$lib/utilz';
+	import toast from 'svelte-5-french-toast';
 
 	let shareName = $state(page.url.searchParams.get('s') || '');
+	let saving = $state(false);
+	let loading = $state(false);
 
 	const allTracks = TREASURE_TROVE.DEMON.concat(
 		TREASURE_TROVE.BONUS,
@@ -13,54 +19,122 @@
 		TREASURE_TROVE.CLICES
 	);
 
+	allTracks.sort((a, b) => {
+		return a.localeCompare(b);
+	});
+
 	/**
 	 * @type {any}
 	 */
 	let glawski = $state({
 		start: 0,
-		duration: 5,
+		loopLength: 19,
+		duration: NaN,
 		title: '',
-		audioFile: ''
+		audioFile: '',
+		paused: false,
+		currentTime: 0
 	});
 
-	let troof: string | null = $state(null);
-	let paused = $state(true);
-
-	let errors: string[] = $state([]);
-	let currentTime = $state(0);
-	let duration = $state(NaN);
-
-	onMount(() => {
+	onMount(async () => {
 		const worker = new Worker('/work_faster.js');
 		worker.onmessage = (e) => {
 			timerLoop();
 		};
 
 		if (!shareName) {
-			return;
+			newGlawski();
+		}
+
+		try {
+			loading = true;
+			const response = await axios.get(`${PUBLIC_BACKEND_BASE_URL}/gatsby/glawskis/${shareName}`);
+			loading = false;
+			glawski = response.data.glawski;
+		} catch (e) {
+			console.error(`Failed to load glawski`, e);
 		}
 	});
 
 	const onTogglePause = () => {
-		paused = !paused;
+		glawski.paused = !glawski.paused;
 	};
 
-	const onSeek = () => {};
+	const onSeek = (newValue: number) => {
+		newValue = Number(newValue.toFixed(1));
+		glawski.currentTime = newValue;
+		glawski.start = newValue;
+
+		capLoopLength();
+	};
 
 	const timerLoop = () => {
-		// if (currentTime > glawski.start + glawski.duration) {
-		// 	currentTime = glawski.start;
-		// }
+		if (glawski.currentTime > glawski.start + glawski.loopLength) {
+			glawski.currentTime = glawski.start;
+		}
 	};
 
 	const newGlawski = () => {
 		shareName = '';
 		glawski.audioFile = allTracks[0];
-		troof = glawski.audioFile;
-		console.log('heh', troof);
 	};
 
-	const saveGlawski = () => {};
+	const saveGlawski = async () => {
+		saving = true;
+		try {
+			const response = await axios.post(`${PUBLIC_BACKEND_BASE_URL}/gatsby/glawskis`, {
+				glawski
+			});
+
+			saving = false;
+			toast.success('Saved (dat sucka)');
+
+			if (response.data.glawski?.shareName) {
+				page.url.searchParams.set('s', response.data.glawski.shareName);
+				pushState(page.url, {});
+			} else {
+				alert('doh');
+			}
+		} catch (e) {
+			toast.error('Damn shame');
+			console.error(`Failed to save glawski`, e);
+			saving = false;
+		}
+	};
+
+	const updateStartValue = (e: Event) => {
+		const value = Number((e.currentTarget as HTMLInputElement).value);
+
+		if (value <= 0 || isNaN(value)) {
+			return;
+		}
+
+		glawski.currentTime = value;
+		glawski.start = value;
+	};
+
+	const updateLoopLength = (e: Event) => {
+		const value = Number((e.currentTarget as HTMLInputElement).value);
+
+		if (value <= 0 || isNaN(value)) {
+			return;
+		}
+
+		glawski.loopLength = value;
+		capLoopLength();
+	};
+
+	const changedTrack = () => {
+		glawski.start = 0;
+		glawski.loopLength = 19;
+		capLoopLength();
+	};
+
+	const capLoopLength = () => {
+		if (glawski.start + glawski.loopLength > glawski.duration) {
+			glawski.loopLength = Number((glawski.duration - glawski.start).toFixed(1));
+		}
+	};
 </script>
 
 <svelte:head>
@@ -69,85 +143,90 @@
 </svelte:head>
 
 <div class="p-5">
-	<div>
-		<button class="bg-green-700 p-2" onclick={newGlawski}>Create Glawski</button>
-	</div>
+	<div class="text-3xl">What The C?...</div>
 
-	<div class="mt-5 text-3xl">What The C?...</div>
-	<audio src={troof} bind:currentTime bind:duration bind:paused></audio>
+	{#if saving}
+		<div class="text-3xl text-slate-500">Saving...</div>
+	{/if}
 
-	{#if shareName}
-		<div class="text-2xl text-slate-500">
-			{glawski?.title}
-		</div>
+	<audio
+		src={glawski.audioFile}
+		bind:currentTime={glawski.currentTime}
+		bind:duration={glawski.duration}
+		bind:paused={glawski.paused}
+		onloadeddata={capLoopLength}
+	></audio>
 
-		<div class="mt-5 text-2xl text-red-500">
-			{#each errors as error}
-				<div>
-					{error}
-				</div>
+	<div class="mt-5">
+		<select
+			class="border border-solid border-slate-500 bg-black p-2 align-middle"
+			bind:value={glawski.audioFile}
+			onchange={changedTrack}
+		>
+			{#each allTracks as track}
+				<option value={track}>{formatTrack(track)}</option>
 			{/each}
-		</div>
+		</select>
 
-		<div>
-			<Wavey {currentTime} {paused} {duration} {onTogglePause} {onSeek} />
-			<!-- subslice={{ start: startTime, end: startTime + loopDuration }} -->
-		</div>
-	{:else}
+		<input
+			type="text"
+			class="ml-3 min-w-128 border border-solid border-slate-500 p-2 align-middle"
+			bind:value={glawski.title}
+			placeholder="Yeh I remembaw..."
+		/>
+
+		<button
+			aria-label="Save"
+			class="align-middle text-blue-400 disabled:text-slate-500"
+			onclick={saveGlawski}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="size-8"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M6 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3H6Zm1.5 1.5a.75.75 0 0 0-.75.75V16.5a.75.75 0 0 0 1.085.67L12 15.089l4.165 2.083a.75.75 0 0 0 1.085-.671V5.25a.75.75 0 0 0-.75-.75h-9Z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</button>
+
+		<Wavey
+			currentTime={glawski.currentTime}
+			paused={glawski.paused}
+			duration={glawski.duration}
+			{onTogglePause}
+			{onSeek}
+			subslice={{ start: glawski.start, end: glawski.start + glawski.loopLength }}
+		/>
+
 		<div class="mt-5">
-			<select class="border border-solid border-slate-500 bg-black p-2 align-middle">
-				{#each allTracks as track}
-					<option value={track}>{formatTrack(track)}</option>
-				{/each}
-			</select>
-			<!-- 
-			<button aria-label="Play" onclick={() => onTogglePause()} class="align-middle">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					class="size-8"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</button> -->
-
-			<Wavey {currentTime} {paused} {duration} {onTogglePause} {onSeek} />
-
-			<div class="mt-5">
-				<div>
+			<div>
+				<div class="inline-block align-middle">
 					Start
 					<input
 						type="text"
 						class="border border-solid border-slate-500 p-2"
 						value={glawski.start}
+						onchange={updateStartValue}
 					/>
+				</div>
 
+				<div class="ml-5 inline-block align-middle">
 					Duration
 					<input
 						type="text"
 						class="border border-solid border-slate-500 p-2"
-						value={glawski.duration}
+						value={glawski.loopLength}
+						onchange={updateLoopLength}
 					/>
-
-					Title
-					<input
-						type="text"
-						class="border border-solid border-slate-500 p-2"
-						value={glawski.title}
-					/>
-				</div>
-
-				<div class="mt-5">
-					<button class="bg-violet-500 p-2" onclick={saveGlawski}>Save Glawski</button>
 				</div>
 			</div>
 		</div>
-	{/if}
+	</div>
 
 	<div class="mt-5">
 		<div>
